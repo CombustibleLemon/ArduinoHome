d69a3ab66d7e096d598ba6e151420a6d4bd455ce
Jefferson Henry <jefferson@yefferson.com>
2014-05-31 20:49:49 -0500

    Added sketch project

---
+++ Arduino/Thermostat.ino
@@ -0,0 +1,32 @@
+
+/*
+   For use with a 20x4 LCD
+*/
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Setup...");
+  Serial.println();
+  
+  lcd.begin(20, 4);
+  lcd.setCursor(1, 0);
+  lcd.print(" === == == == === ");
+  lcd.setCursor(1, 1);
+  lcd.print(": I'M A REAL BOY :");
+  lcd.setCursor(1, 2);
+  lcd.print(": YOUNG COCONUTS :");
+  lcd.setCursor(1, 3);
+  lcd.print(" === == == == === ");
+  delay(2000);
+  
+  //TODO: More setup
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

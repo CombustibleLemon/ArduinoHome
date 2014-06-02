f529f85e1dc5adcc4068076995d33f9e67e5598c
Starbuck Johnson <starbuck@modwiz.com>
2014-06-02 07:55:40 -0500

    Added temperature libs
    And fixed that stupid "); missing problem

--- Arduino/Thermostat/Thermostat.ino
+++ Arduino/Thermostat/Thermostat.ino
@@ -4,13 +4,16 @@
 */
 
 #include <LiquidCrystal.h>
+#include "DallasTemperature.h"
+#include "temperature.h"
+
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Setup...");
-  
+  initThermometer();
   lcd.begin(20, 4);
   lcd.setCursor(1, 0);
   lcd.print(" === == == == === ");
@@ -22,10 +25,21 @@ void setup() {
   lcd.print(" === == == == === ");
   delay(2000);
   
-  Serial.println("
+  Serial.println("");
 }
 
 void loop() {
+  lcd.setCursor(1,3);
+  lcd.print("Inside Temp: ");
+  float insideTemp = getTempF();
+  lcd.print(insideTemp);
+  lcd.print("F");
+  if (insideTemp > 75) {
+    // Turn on A/C
+  } else {
+    // Turn off A/C
+  }
+
   // put your main code here, to run repeatedly:
 
 }
